e2854966779735fb25a8bbd3104420b4b97b2725
Peter Hurford <peter@peterhurford.com>
2016-07-22 05:49:12 -0500

    Finish tests

--- R/fn.R
+++ R/fn.R
@@ -8,10 +8,14 @@ fn <- function(...) {
   args <- eval(substitute(alist(...)))
   body <- tail(args, 1)[[1]]
   formals <- head(args, -1)
-  #TODO: formals with arguments require looking at names.
   inner_fn <- function() { }
   a <- replicate(length(formals), formals(function(x) {})[[1]])
-  names(a) <- formals
+  if (named(formals)) {
+    names(a) <- lapply(c(setdiff(names(formals), ""), formals[names(formals) == ""]), as.name)
+    a <- merge(filter(formals, Negate(is.name)), a)
+  } else {
+    names(a) <- formals
+  }
   formals(inner_fn) <- a
   body(inner_fn) <- body
   inner_fn

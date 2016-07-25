c960c8c474eeebc9239d867b28051d29b479b53b
Peter Hurford <peter@peterhurford.com>
2016-07-25 03:16:48 -0500

    Started export_lsplit_wsplit

--- R/splitters.R
+++ R/splitters.R
@@ -1,2 +1,9 @@
+#' Split a string into a list by letters.
+#' @param str character. The string to split.
+#' @export
 lsplit <- function(str) { strsplit(str, "")[[1]] }
+
+#' Split a string into a list by words.
+#' @param str character. The string to split.
+#' @export
 wsplit <- function(str) { strsplit(str, " ")[[1]] }

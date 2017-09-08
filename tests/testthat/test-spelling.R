a187ab97cdc39e3015fa22bfa0f920ee836dcc7c
peterhurford <peter@peterhurford.com>
2017-09-08 00:16:10 -0500

    Find package root

--- tests/testthat/test-spelling.R
+++ tests/testthat/test-spelling.R
@@ -1,6 +1,7 @@
 context("spelling")
 test_that("Package is spelled correctly", {
-  spellcheck <- spelling::spell_check_package("../..")
+  root <- rprojroot::find_root("DESCRIPTION")
+  spellcheck <- spelling::spell_check_package(root)
   expect_equal(nrow(spellcheck), 0,
                info = paste("Misspelled words:", paste0(spellcheck$word, collapse = ", ")))
 })
